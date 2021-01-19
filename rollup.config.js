1e96c941eb8356bdbf568bbdfab70a7569783d6c
sashka neyman <neyman.sashka@gmail.com>
2021-01-19 11:12:05 +0300

    initial

---
+++ rollup.config.js
@@ -0,0 +1,51 @@
+import copy from 'rollup-plugin-copy';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
+const assets = {
+	targets: [{ src: ['static/index.html', 'static/style.css'], dest: 'dist' }],
+};
+
+function watchstatic(options = {}) {
+	const { targets = [] } = options;
+
+	return {
+		name: 'watchstatic',
+		buildStart() {
+			for (const target of targets) {
+				this.addWatchFile(target.src);
+			}
+		},
+	};
+}
+
+export default {
+	input: 'src/index.ts',
+	output: {
+		file: 'dist/bundle.js',
+		sourcemap: true,
+		format: 'iife',
+	},
+	plugins: [
+		serve({
+			contentBase: 'dist/',
+			host: 'localhost',
+			port: 8080,
+		}),
+		livereload({
+			exts: ['html', 'js', 'less', 'css'],
+			watch: './dist/',
+			port: 500,
+			delay: 200,
+		}),
+		typescript({ lib: ['es5', 'es6', 'dom'], target: 'es6', sourceMap: true }),
+		resolve(),
+		commonjs(),
+		copy(assets),
+		watchstatic(assets),
+	],
+};
